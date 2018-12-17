8b749f7fa54f9420bc373e01d42343da4d7a5dc1
muhdmirzamz <muhdmirzamz@gmail.com>
2018-12-17 19:57:03 +0800

    Initial commit

---
+++ Twit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twit
+//
+//  Created by Muhd Mirza on 17/12/18.
+//  Copyright © 2018 muhdmirzamz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
